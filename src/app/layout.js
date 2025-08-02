cb16344ba9974f37f345976c1a51e07252424ebb
almightyprogrammer <lehman0308@gmail.com>
2025-08-03 01:22:56 +1200

    vercel web tracking component added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'; // Your global styles
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-
+import { Analytics } from "@vercel/analytics/next"
 export const metadata = {
   title: 'Juwon Han Developer Portfolio',
   description: 'A short run down of my professional career.',
@@ -14,6 +14,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${GeistSans.className} graph-grid-background`}>
         {children}
+        <Analytics />
       </body>
     </html>
   );
