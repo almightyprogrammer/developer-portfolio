8ecaabcaf69f13cb2cb5a134b5159565cb0f36d5
almightyprogrammer <lehman0308@gmail.com>
2025-08-02 23:59:05 +1200

    metadata

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,18 @@
 import './globals.css'; // Your global styles
 import { GeistSans } from 'geist/font/sans';
 import { GeistMono } from 'geist/font/mono';
-// This is the correct way to define and export the component
+
+export const metadata = {
+  title: 'Juwon Han Developer Portfolio',
+  description: 'A short run down of my professional career.',
+};
+
 export default function RootLayout({
   children,
 }) {
   return (
     <html lang="en">
-      <body className={`${GeistSans.className} graph-grid-background mx-2`}>
+      <body className={`${GeistSans.className} graph-grid-background`}>
         {children}
       </body>
     </html>
